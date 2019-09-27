e809ce48aa05b51dbd42b3f9c3f15de4044fcf36
kepo4ka <konstantin.bugulov@mail.ru>
2019-09-27 17:32:31 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,17 +6,21 @@ function fetch($url, $z = null)
     try {
         $ch = curl_init();
 
+        if (!empty($z['params'])) {
+            $url .= '?' . http_build_query($z['params']);
+        }
+
         $useragent = isset($z['useragent']) ? $z['useragent'] : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:10.0.2) Gecko/20100101 Firefox/10.0.2';
 
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_AUTOREFERER, true);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_POST, isset($z['post']));
-
-        if (isset($z['post'])) {
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $z['post']);
-        }
+//        curl_setopt($ch, CURLOPT_POST, isset($z['params']));
+//
+//        if (isset($z['post'])) {
+//            curl_setopt($ch, CURLOPT_POSTFIELDS, $z['params']);
+//        }
 
         if (isset($z['refer'])) {
             curl_setopt($ch, CURLOPT_REFERER, $z['refer']);
@@ -44,3 +48,4 @@ function echoVarDumpPre($var)
     echo '</pre>';
     exit;
 }
+
