ca621d1e5d10885b2eaf3574d9054cff63b7cc08
kepo4ka <konstantin.bugulov@mail.ru>
2020-04-23 12:02:23 +0300

    Create covid.php

---
+++ covid.php
@@ -0,0 +1,23 @@
+<?php
+
+include __DIR__ . '/init.php';
+
+$user = 'root';
+$pass = '';
+$db_name = 'coronovirus';
+$host = 'localhost';
+
+$db = new SafeMysql(array('host' => $host, 'user' => $user, 'pass' => $pass, 'db' => $db_name, 'charset' => 'utf8'));
+
+$project_url = 'http://localhost/';
+
+$proxy_list = ProxyDB::getList();
+
+ProxyDB::update();
+
+
+getVisaInfo();
+getAirports();
+
+
+
